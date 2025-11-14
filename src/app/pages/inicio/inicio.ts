0ad2d62872481a6a45e0e90c336d35806c53ee1d
Danny <dannyhuamanzavala16@gmail.com>
2025-11-14 08:43:30 -0500

    Otro commit

---
+++ src/app/pages/inicio/inicio.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { zip } from 'rxjs';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [],
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.css',
+})
+export class Inicio {
+
+  
+}
